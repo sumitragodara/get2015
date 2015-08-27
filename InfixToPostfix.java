de1c8da9d90c2959c6b81c6d00f0797efb01c166
sumitra godara <sumitragodara@users.noreply.github.com>
2015-08-27 17:47:58 +0530

    Update InfixToPostfix.java

--- InfixToPostfix.java
+++ InfixToPostfix.java
@@ -50,6 +50,39 @@ public class InfixToPostfix {
 	 */
 	public void infixToPostfix (String infix) {
 		Stack stack = new Stack();
+		/**
+		 * check that operator/operand and next to that operator/operand are same or not...if same throw exception
+		 */
+		try{
+		for(int i = 0; i < (infix.length()-1) ; i++)
+		{
+			if(isOperator(infix.charAt(i))){
+				if(isOperator(infix.charAt(i+1))){
+					throw new Exception("operators are coming back to back");
+				}
+					}
+			
+			if(isOperand(infix.charAt(i))){
+				if(isOperand(infix.charAt(i+1))){
+					throw new Exception("operands are coming back to back");
+				}
+			}
+		}
+		/**
+		 * check that open and closing brackets
+		 */
+		char value;
+		int openBreaket = 0,closeBreaket = 0;
+		for(int i = 0;i<infix.length();i++){
+			value=infix.charAt(i);
+			if(value == '(')
+				openBreaket++;
+			else if(value ==')')
+				closeBreaket++;	
+		}
+		if(openBreaket !=closeBreaket)
+			throw new Exception("open and closing brackets are not equal");
+	
 		stack.pushItem('(');
 		for(int count = 0; count < infix.length()+1; count++) {
 			char currentChar ;
@@ -58,13 +91,17 @@ public class InfixToPostfix {
 			} else {
 				currentChar = infix.charAt(count);
 			}
-			
+			if(currentChar =='('){
+				stack.pushItem(currentChar);
+			}
 			if(isOperand(currentChar)) {
 				System.out.print("" + currentChar);
 			} else if(isOperator(currentChar)) {
-				if(priority(currentChar) < priority((char)Stack.top.data)) {
+				if(priority(currentChar) <= priority((char)Stack.top.data)) {
 					System.out.print("" + (char)Stack.top.data);
 				 stack.popItem();	
+				 count=count-1;
+					continue;
 				} else {
 					stack.pushItem(currentChar);
 				}
@@ -72,11 +109,23 @@ public class InfixToPostfix {
 				while((char)Stack.top.data != '(') {
 					System.out.print("" + (char)Stack.top.data);
 					stack.popItem();
+				
 				}
-			} else {
-				System.out.println("invalid infix");
-			}
+			} 
+			
 		}
+		while(Stack.top != null){
+			if((char)Stack.top.data == '(')
+				stack.popItem();
+			else{
+			System.out.print("" + (char)Stack.top.data);
+			 stack.popItem();
+		}}
+	}catch (Exception e) {
+		
+		//System.out.println("invalid string");
+		System.out.println(e);
+	}
 	}
 	/*
 	 * main method
