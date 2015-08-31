14b0de664c91c311b5f963e700a577ab054a91cd
sumitra godara <sumitragodara@users.noreply.github.com>
2015-08-31 15:34:46 +0530

    Update RoomAllocation.java

--- RoomAllocation.java
+++ RoomAllocation.java
@@ -1,11 +1,19 @@
 import java.util.*;
-
+/*
+ * this class is use to allocate hotel rooms according to hash function
+ * according to question here we are using hash function of age of guest
+ * @param rooms is total no of rooms in hotel
+ * @count is counter how many room allocated
+ */
 public class RoomAllocation{
 	static Scanner scan = new Scanner(System.in);
 	HashMap<Integer, String> hotelRoom = new HashMap<Integer, String>();
 	static int rooms=17;
 	static int count= 0;
-	
+	/*
+	 * this method is main method of class RoomAllocation
+	 * this take choice from user what he want to perform according to menu 
+	 */
 	public static void main(String []args){
 		RoomAllocation roomAllocation = new RoomAllocation();
 		roomAllocation.createMap();
@@ -20,7 +28,7 @@ public class RoomAllocation{
 					int age = scan.nextInt();
 					System.out.println("Enter Name of person:");
 					String name = scan.next();
-					roomAllocation.alloteRoom(name,age);
+					roomAllocation.allotRoom(name,age);
 				}
 				else{
 					System.out.println("No Room Available");
@@ -39,17 +47,27 @@ public class RoomAllocation{
 		}
 		
 	}
+	/*
+	 * this method create empty hash map data structure
+	 * empty is specified by vacant room
+	 */
 	private void createMap(){
 		for(int i=0;i<rooms;i++){
-			hotelRoom.put(i,"vacent");
+			hotelRoom.put(i,"vacant");
 		}
 	}
+	/*
+	 * this method is to allot room to guest according to their age
+	 * it assign room according to hash function if vacant otherwise it allot next vacant room
+	 * @param key index or key of hash map
+	 * @param flag to room allocate to guest or not
+	 */
 	
-	private void alloteRoom(String name,int age) {
+	private void allotRoom(String name,int age) {
 		int key = age%rooms;
 		boolean flag = false;
 		while(!flag){
-			if(hotelRoom.get(key).equalsIgnoreCase( "vacent")){
+			if(hotelRoom.get(key).equalsIgnoreCase( "vacant")){
 				hotelRoom.put(key, name);
 				flag=true;
 				count++;
@@ -64,7 +82,11 @@ public class RoomAllocation{
 		
 		
 	}
-
+/*
+ * this method provide the list of all rooms in hotel
+ * either it is vacant or alloted to someone
+ * if occupied then it show name of person whom room alloted
+ */
 	public void viewList() {
 		for(int i =0;i<rooms;i++){
 			if(hotelRoom.get(i).equalsIgnoreCase( "vacent")){
